6c9b4363340e08f3cc71860a2782a16974f89989
kathedavila <katherine.davila@gmail.com>
2014-08-18 15:21:12 -0500

    assss

---
+++ src/Suma4.java
@@ -0,0 +1,17 @@
+
+public class Suma4 {
+
+	private int num1;
+
+	   private int num2;
+
+	   public Suma4(int n1, int n2) {
+	       num1 = n1;
+	       num2 = n2;
+	   }
+	   public int sumar() {
+	       int resultado = num1 + num2;
+	       return resultado;
+	   }
+	
+}
